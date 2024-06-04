e3a76e67fed72793b6b01dc4c8653db41e193ad1
Miha Jan Strehovec <48392708+mihajanstrehovec@users.noreply.github.com>
2024-06-04 07:56:24 +0200

    code refactoring

--- src/app/hooks/useMousePosition.tsx
+++ src/app/hooks/useMousePosition.tsx
@@ -5,44 +5,37 @@ export const useMousePosition = (headshot:any) => {
     const [position, setPosition] = useState("centerright")
 
     useEffect(()=>{
-        
         const headshot = document.getElementById("headshot")
         const headShotPosition = headshot?.getBoundingClientRect()
         //@ts-ignore
-        console.log(headShotPosition)
         const setFromEvent = (e) => {
-            
-            
-
-            if(((headShotPosition.y + headShotPosition.height) - e.clientY) > 0){
-                if(e.clientY >= headShotPosition.y && e.clientY <= (headShotPosition.y + headShotPosition.height)){
-                    if(((headShotPosition.x + headShotPosition.width/2) - e.clientX) > 0){
-                        setPosition("centerleft")
+            if(headShotPosition){
+                if(((headShotPosition.y + headShotPosition.height) - e.clientY) > 0){
+                    if(e.clientY >= headShotPosition.y && e.clientY <= (headShotPosition.y + headShotPosition.height)){
+                        if(((headShotPosition.x + headShotPosition.width/2) - e.clientX) > 0){
+                            setPosition("centerleft")
+                        } else if(((headShotPosition.x + headShotPosition.width/2) - e.clientX) < 0){
+                            setPosition("centerright")
+                        }
+                    } else if(((headShotPosition.x + headShotPosition.width/2) - e.clientX) > 0){
+                        setPosition("topleft")
                     } else if(((headShotPosition.x + headShotPosition.width/2) - e.clientX) < 0){
-                        setPosition("centerright")
+                        setPosition("topright")
                     }
-                } else if(((headShotPosition.x + headShotPosition.width/2) - e.clientX) > 0){
-                    setPosition("topleft")
-                } else if(((headShotPosition.x + headShotPosition.width/2) - e.clientX) < 0){
-                    setPosition("topright")
-                }
-            } else if(((headShotPosition.y + headShotPosition.height) - e.clientY) <= 0){
-                if(((headShotPosition.x + headShotPosition.width/2) - e.clientX) > 0){
-                    setPosition("bottomleft")
-                } else if(((headShotPosition.x + headShotPosition.width/2) - e.clientX) < 0){
-                    setPosition("bottomright")
-                }
-            } else {
-                if(((headShotPosition.x + headShotPosition.width/2) - e.clientX) > 0){
-                    setPosition("bottomleft")
-                } else if(((headShotPosition.x + headShotPosition.width/2) - e.clientX) < 0){
-                    setPosition("bottomright")
-                }
+                } else if(((headShotPosition.y + headShotPosition.height) - e.clientY) <= 0){
+                    if(((headShotPosition.x + headShotPosition.width/2) - e.clientX) > 0){
+                        setPosition("bottomleft")
+                    } else if(((headShotPosition.x + headShotPosition.width/2) - e.clientX) < 0){
+                        setPosition("bottomright")
+                    }
+                } else {
+                    if(((headShotPosition.x + headShotPosition.width/2) - e.clientX) > 0){
+                        setPosition("bottomleft")
+                    } else if(((headShotPosition.x + headShotPosition.width/2) - e.clientX) < 0){
+                        setPosition("bottomright")
+                    }
+                }    
             }
-
-            
-
-            
         } 
         document.addEventListener("mousemove", setFromEvent)
 
